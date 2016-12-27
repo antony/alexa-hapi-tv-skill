4a04a599b01d2e5180e297a04eedd9fa0199d120
Antony Jones <aj@desirableobjects.co.uk>
2016-12-27 20:19:09 +0000

    Initial Commit

---
+++ test/alexa-request.builder.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const basePayload = require('./request.json')
+const intentPayload = require('./intent-request.json')
+
+class AlexaRequestBuilder {
+
+  constructor (request) {
+    this.request = basePayload
+    this.request.request = intentPayload
+  }
+
+  to (connection, endpoint) {
+    return connection.injectThen({
+      method: 'POST',
+      url: endpoint,
+      payload: this.request
+    })
+  }
+}
+
+module.exports = {
+  withIntent: () => { return new AlexaRequestBuilder(intentPayload) } 
+}
---
+++ test/functional.test.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const Code = require('code')
+const Lab = require('lab')
+const lab = exports.lab = Lab.script()
+
+const describe = lab.describe
+const context = lab.describe
+const it = lab.it
+const before = lab.before
+const after = lab.after
+const expect = Code.expect
+
+const AlexaRequestBuilder = require('./alexa-request.builder')
+
+const server = require('..')
+
+describe('alexa-tv', () => {
+  before((done) => {
+    done()
+  })
+
+  after((done) => {
+    done()
+  })
+
+  context('TV on reqest', () => {
+    it('Turns the tv on', (done) => {
+      return AlexaRequestBuilder
+        .withIntent()
+        .to(server, server.info.uri + '/')
+        .then((output) => {
+          expect(output.statusCode).to.equal(200)
+          expect(output.result.response.outputSpeech.ssml).to.equal('<speak>You asked for the tv to be turned on</speak>')
+        })
+    })
+  })
+})
